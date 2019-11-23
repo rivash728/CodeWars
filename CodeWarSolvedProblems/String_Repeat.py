50fe739deb5eb2411e5139e032ff0646b0e867b6
rivash728 <35610411+rivash728@users.noreply.github.com>
2019-11-23 18:37:47 -0500

    An exercise using operators on strings

---
+++ CodeWarSolvedProblems/String_Repeat.py
@@ -0,0 +1,14 @@
+# String_Repeat.py
+# Author: Henry Rivas
+# Date: 11/23/2019
+# Desc: 8th Kyu Problem in Code Wars! Define
+#       a method that repeats a string x amount
+#       of times
+
+def repeat (num, string):
+    return string * num
+
+def main():
+    print(repeat(5,'Hello!'))
+
+main()
