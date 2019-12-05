efa74c2f9fe0c110de27a1d70a9c9e76db9addc9
rivash728 <35610411+rivash728@users.noreply.github.com>
2019-12-04 20:48:22 -0500

    Updated CodeWars Solutions
    A program that uses knowledge of int and float manipulation.

---
+++ CodeWarSolvedProblems/Hydrated.py
@@ -0,0 +1,14 @@
+# Hydrated.py
+# Author: Henry Rivas
+# Date: 12/4/2019
+# Desc: 8th Kyu Problem in Code Wars!
+
+def litres(time):
+    litres = ( 0.5 * time )
+    return int(litres)
+
+def main():
+
+    print(litres(1.4))
+
+main()
